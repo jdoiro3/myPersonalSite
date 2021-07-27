d2d8c9b82b5502b5d391c14554f2f0f5a8d02742
Joe Doiron <jdoiron2@unum.com>
2021-07-27 10:41:19 -0400

    changed url patterns and styling

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'blog'
 urlpatterns = [
-    path('', views.index, name='blog-index'),
+    path('', views.index, name='index'),
     path('cat/<str:category>/', views.index, name='index-specific-cat'),
     path('author/<str:author_first_name>-<str:author_last_name>/', views.author_index, name='specific-author'),
     path('post/<slug:slug>/', views.post_detail, name='post-detail'),
