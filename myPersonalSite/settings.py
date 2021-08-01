b9feb2a3bec7f0c12aa8ab8418fc08d0a0332502
Joseph Doiron <josephdoiron1234@yahoo.com>
2021-08-01 18:54:49 -0400

    django-cleanup should be last in installed apps

--- myPersonalSite/settings.py
+++ myPersonalSite/settings.py
@@ -53,8 +53,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_cleanup.apps.CleanupConfig',
     'storages',
+    'django_cleanup.apps.CleanupConfig',
 ]
 
 MIDDLEWARE = [
