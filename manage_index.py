fb3e94fedb3a8dfe34abd618a7e27c11921f1f62
Joe Doiron <jdoiron2@unum.com>
2021-09-13 14:16:26 -0400

    changed args. Conflicted with Heroku.

--- manage_index.py
+++ manage_index.py
@@ -5,8 +5,8 @@ commands = ["cleanup", "re-index"]
 
 parser = argparse.ArgumentParser(description='Manager for the Inverted Index.')
 parser.add_argument('command', choices=commands, help='Command to perform on index.')
-parser.add_argument('-s3', '--in_s3', action='store_true', help='If passed, the index will be loaded from the S3 bucket')
-parser.add_argument('-fp', '--file_path', nargs='?', const='index.json', help='The file path for the index.')
+parser.add_argument('--in_s3', action='store_true', help='If passed, the index will be loaded from the S3 bucket')
+parser.add_argument('--file_path', nargs='?', const='index.json', help='The file path for the index.')
 
 args = parser.parse_args()
 inv_index = index.InvertedIndex(from_file=True, in_s3=args.in_s3, file_path=args.file_path or 'index.json')
